0e4a45db7c9c39a0d8346b9fcb8781de34500131
Ashton Dunderdale <121163457+ashtondunderdale@users.noreply.github.com>
2023-11-07 16:23:26 +0000

    added sample text, formatted elements

--- scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const backgroundColors = ["#FFEBD7", "#D7E4F3"];
-
-    function setRandomBackgroundColor() {
-        const randomIndex = Math.floor(Math.random() * backgroundColors.length);
-        document.body.style.backgroundColor = backgroundColors[randomIndex];
-    }
-    setRandomBackgroundColor();
-});
