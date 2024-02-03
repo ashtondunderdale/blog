7d956a2f5e114bcd9bf3435c9787750ee067ebae
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-02-03 17:17:46 +0000

    added mouse region

--- blog/lib/widgets/filter.dart
+++ blog/lib/widgets/filter.dart
@@ -1,7 +1,7 @@
 import 'package:blog/utils/styles.dart';
 import 'package:flutter/material.dart';
 
-class FilterOption extends StatelessWidget {
+class FilterOption extends StatefulWidget {
   const FilterOption({Key? key, required this.name, required this.onTap, required this.isSelected, required this.hasActivePost});
 
   final String name;
@@ -10,30 +10,44 @@ class FilterOption extends StatelessWidget {
   final bool hasActivePost;
 
   @override
+  _FilterOptionState createState() => _FilterOptionState();
+}
+
+class _FilterOptionState extends State<FilterOption> {
+  bool isHovered = false;
+
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onTap,
-      child: Padding(
-        padding: const EdgeInsets.only(left: 8, top: 8),
-        child: SizedBox(
-          child: Align(
-            alignment: Alignment.centerLeft,
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 8),
-                  child: hasActivePost ? const Icon(
-                    Icons.arrow_back,
-                    size: 12,
-                  ) : const SizedBox(),
-                ),
-                Text(
-                  name,
-                  style: filterStyle.copyWith(
-                    fontWeight: isSelected || hasActivePost ? FontWeight.bold : FontWeight.normal,
+      onTap: widget.onTap,
+      child: MouseRegion(
+        onEnter: (_) => setState(() => isHovered = true),
+        onExit: (_) => setState(() => isHovered = false),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 8, top: 8),
+          child: SizedBox(
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8),
+                    child: widget.hasActivePost
+                        ? const Icon(
+                            Icons.arrow_back,
+                            size: 12,
+                          )
+                        : const SizedBox(),
                   ),
-                ),
-              ],
+                  Text(
+                    widget.name,
+                    style: filterStyle.copyWith(
+                      fontWeight: widget.isSelected || widget.hasActivePost ? FontWeight.bold : FontWeight.normal,
+                      color: isHovered ? Color.fromARGB(255, 121, 121, 121) : Colors.black, // Change text color on hover
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
